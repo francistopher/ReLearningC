109e90c413c2a29590ca573499e2446e8d9e0557
francistopher <francistopherr@gmail.com>
2021-04-23 22:48:29 -0400

    UPDATE Reduce 2ascii.c

--- 01/2ascii.c
+++ 01/2ascii.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
 /*
  * stores the first 32 ascii values
@@ -70,124 +71,6 @@ void printTable(void) {
 }
 
 /*
- * prompts the user to enter the ascii text
- * corresponding to every decimal value
- */
-void enterTextGivenDecimal(void) {
-    printf("\nBuild the ASCII table given the decimal values!");
-    unsigned short i;
-    printf("\n|");
-    printBlanks(24);
-    printf("ASCII Table");
-    printBlanks(24);
-    printf(" |\n");
-    char txt[26];
-    char in[26];
-    // prints the decimal values and the corresponding ascii characters
-    for(i = 0; i < 128; i++) {
-        // setup number of blank spaces in row
-        unsigned short strLength = 29;
-        if (i < 33) {
-            strLength = 30 - strlen(table[i]);
-        } else if (i == 127) {
-            strLength = 24;
-        }
-        // print the decimal values
-        printf("|%15d", i);
-        printBlanks(14);
-        printf("|");
-        printBlanks(strLength / 2);
-        // add blank space for odd length characters
-        if (strLength % 2 == 1) {
-            printf(" ");
-        }
-        // print the ascii characters
-        if (i < 33) {
-            strncpy(txt, table[i], 26);
-        } else {
-            if (i != 127) {
-                sprintf(txt, "%c", i);
-            } else {
-                strncpy(txt, "DELETE", 26);
-            }
-        }
-        fflush(stdout);
-        scanf("%20s", &in);
-        // Compare the strings
-        if (strcmp(txt, in) != 0) {
-            printf("INCORRECT, THE ANSWER IS >>> %s\n", txt);
-            printf("YOU LOSE!\n");
-            break;
-        }
-    }
-    if (strcmp(txt, in) == 0) {
-        printf("\nCONGRATULATIONS, YOU'VE ENTERED ALL THE CHARACTERS");
-        printf("\nGIVEN THEIR CORRESPONDING DECIMAL VALUES CORRECTLY!\n");
-    }
-}
-
-/*
- * prompts the user to enter the decimal value 
- * corresponding to every ascii character
- */
-void enterDecimalGivenText(void) {
-    printf("Build the ASCII table given the characters!");
-    unsigned short i;
-    printf("\n|");
-    printBlanks(24);
-    printf("ASCII Table");
-    printBlanks(24);
-    printf(" |\n");
-    int in = -1;
-    // prints the decimal values and the corresponding ascii characters
-    for(i = 0; i < 128; i++) {
-        // setup number of blank spaces in row
-        unsigned short strLength = 29;
-        if (i < 33) {
-            strLength = 30 - strlen(table[i]);
-        } else if (i == 127) {
-            strLength = 24;
-        }
-        // print the decimal values
-        printf("|");
-        printBlanks(strLength / 2);
-        // add blank space for odd length characters
-        if (strLength % 2 == 1) {
-            printf(" ");
-        }
-        // print the ascii characters
-        if (i < 33) {
-            printf(table[i]);
-        } else {
-            if (i != 127) {
-                printf("%c", i);
-            } else {
-                printf("DELETE");
-            }
-        }
-        printBlanks(strLength / 2);
-        printf("|");
-        if (i < 10) {
-            printBlanks(14);
-        } else if (i < 100) {
-            printBlanks(13);
-        } else {
-            printBlanks(12);
-        }
-        fflush(stdout);
-        scanf("%d", &in);
-        if (in != i) {
-            printf("INCORRECT, THE ANSWER IS >>> %d\n", i);
-            printf("YOU LOSE!\n");
-            break;
-        }
-    }
-    if (in == (i-1)) {
-        printf("\nCONGRATULATIONS, YOU'VE ENTERED ALL THE DECIMAL INTEGERS");
-        printf("\nGIVEN THEIR CORRESPONDING CHARACTER VALUES CORRECTLY!\n");
-    }
-}
-/*
  * prints instructions for the ascii program
  */
 void printInstructions(void) {
@@ -200,12 +83,108 @@ void printInstructions(void) {
     printf("|%7s      |%13s  |\n", "4", "Text to Dec");
 }
 
+void printTableHeader() {
+    printf("\n|");
+    printBlanks(24);
+    printf("ASCII Table");
+    printBlanks(24);
+    printf(" |\n");
+}
+
+char * getTableInstructions(unsigned char f, unsigned char t) {
+    if (f == 0 && t == 1) {
+        return "Build the ASCII table by entering\nthe Characters given the Decimal Values!";
+    } else {
+        return "Build the ASCII table by entering\nthe Decimal Values given the Characters!";
+    }
+}
+
+void promptUser(unsigned char f, unsigned char t) {
+    unsigned short i = 0;
+    printf(getTableInstructions(f, t));
+    printTableHeader();
+    char in[26];
+    char ans[26];
+    unsigned short strLen = 29;
+    for (int i = 77; i < 128; i++) {
+        // determine blank spacing before and after given
+        if (f == 0 && t == 1) {
+            sprintf(in, "%d", i);
+            strLen = 30 - strlen(in);
+        } else if (f == 1 && t == 0) {
+            if (i < 33) {
+                strLen = 30 - strlen(table[i]);
+            } else if (i == 127) {
+                strLen = 24;
+            } else {
+                sprintf(in, "%c", i);
+                strLen = 30 - strlen(in);
+            }
+        }
+        printf("|");
+        printBlanks(strLen / 2);
+        // add blank space for odd length
+        if (strLen % 2 == 1) {
+            printf(" ");
+        }
+        // print the given
+        if (f == 0 && t == 1) {
+            printf("%d", i);
+        } else if (f == 1 && t == 0) {
+            if (i < 33) {
+                printf(table[i]);
+            } else {
+                if (i != 127) {
+                    printf("%c", i);
+                } else {
+                    printf("DELETE");
+                }
+            }
+        }
+        printBlanks(strLen / 2);
+        printf("|");
+        // determine blank spacing before and after answer 
+        if (f == 0 && t == 1) {
+            if (i < 33) {
+                strncpy(ans, table[i], 26);
+            } else {
+                if (i != 127) {
+                    sprintf(ans, "%c", i);
+                } else {
+                    strncpy(ans, "DELETE", 26);
+                }
+            }
+            strLen = 30 - strlen(ans);
+        } else if (f == 1 && t == 0) {
+            sprintf(ans, "%d", i); 
+            strLen = 30 - strlen(ans);
+        }
+        printBlanks(strLen / 2);
+        fflush(stdout);
+        scanf("%s", &in);
+        // check if input is incorrect
+        if (strcmp(ans, in) != 0) {
+            printf("INCORRECT, THE ANSWER IS >>> %s\nYOU LOSE\n", ans);
+            break;
+        }
+    }
+
+    if (strcmp(ans, in) == 0) {
+        if (f == 0 && t == 1) {
+            printf("\nCONGRATULATIONS, GIVEN THE CORRESPONDING DECIMAL VALUES");
+            printf("\nYOU'VE ENTERED ALL THE CHARACTERS CORRECTLY!");
+        } else if (f == 1 && t == 0) {
+            printf("\nCONGRATULATIONS, GIVEN THE CORRESPONDING CHARACTERS");
+            printf("\nYOU'VE ENTERED ALL THE DECIMAL VALUES CORRECTLY!");
+        }
+    }
+}   
+
 /*
  * executes the program
  */
 int main(void) {
-    printf("Welcome to the ASCII C program!\n");
-    printf("Created by Christopher Francisco!\n");
+    printf("Welcome to the ASCII C program!\nCreated by Christopher Francisco!\n");
     unsigned char in = 1;
     do {
         if (in == 1) {
@@ -213,16 +192,15 @@ int main(void) {
         } else if (in == 2) {
             printTable();
         } else if (in == 3) {
-            enterTextGivenDecimal();
+            promptUser(0, 1);
         } else if (in == 4) {
-            enterDecimalGivenText();
+            promptUser(1, 0);
         }
         printf("\nEnter a command: ");
         fflush(stdout);
         scanf("%d", &in);
         fflush(stdin);
     } while(in != 0);
-    printf("\n...Exiting the ASCII C Program\n");
-    printf("Created by Christopher Francisco\n");
+    printf("\n...Exiting the ASCII C Program\nCreated by Christopher Francisco\n");
     return 0;
 }
